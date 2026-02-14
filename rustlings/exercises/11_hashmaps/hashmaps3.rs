227b26a8a93596c897ac937cf99813d10b97bb18
samuelea <batonenbois@gmail.com>
2026-02-14 17:27:32 -0500

    completed exercises 9, 10, 11

--- rustlings/exercises/11_hashmaps/hashmaps3.rs
+++ rustlings/exercises/11_hashmaps/hashmaps3.rs
@@ -31,6 +31,35 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
+        let new_team_1 = TeamScores {
+            goals_conceded: team_2_score
+                + match scores.get(team_1_name) {
+                    Some(n) => n.goals_conceded,
+                    None => 0,
+                },
+            goals_scored: team_1_score
+                + match scores.get(team_1_name) {
+                    Some(n) => n.goals_scored,
+                    None => 0,
+                },
+        };
+
+        scores.insert(team_1_name, new_team_1);
+
+        let new_team_2 = TeamScores {
+            goals_conceded: team_1_score
+                + match scores.get(team_2_name) {
+                    Some(n) => n.goals_conceded,
+                    None => 0,
+                },
+            goals_scored: team_2_score
+                + match scores.get(team_2_name) {
+                    Some(n) => n.goals_scored,
+                    None => 0,
+                },
+        };
+
+        scores.insert(team_2_name, new_team_2);
     }
 
     scores
@@ -54,9 +83,11 @@ England,Spain,1,0";
     fn build_scores() {
         let scores = build_scores_table(RESULTS);
 
-        assert!(["England", "France", "Germany", "Italy", "Poland", "Spain"]
-            .into_iter()
-            .all(|team_name| scores.contains_key(team_name)));
+        assert!(
+            ["England", "France", "Germany", "Italy", "Poland", "Spain"]
+                .into_iter()
+                .all(|team_name| scores.contains_key(team_name))
+        );
     }
 
     #[test]
