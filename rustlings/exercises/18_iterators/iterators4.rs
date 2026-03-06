a5e114289a5d59bcf91a1f65b1a38700e186ed8b
samuelea <batonenbois@gmail.com>
2026-03-05 21:14:15 -0500

    rustlings iterator 4 exercise

--- rustlings/exercises/18_iterators/iterators4.rs
+++ rustlings/exercises/18_iterators/iterators4.rs
@@ -10,6 +10,7 @@ fn factorial(num: u64) -> u64 {
     // - additional variables
     // For an extra challenge, don't use:
     // - recursion
+    (0..num).fold(1, |acc, b| acc * (b + 1))
 }
 
 fn main() {
